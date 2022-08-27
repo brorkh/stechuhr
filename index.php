3d501069295d5f3ab53bc5626f0dcdbb21f43cd9
Tom <brorkh@users.noreply.github.com>
2022-08-27 13:05:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" href="./apple-touch-icon.png"/>
+<title>Stechuhr</title>
 <meta charset="UTF-8">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
