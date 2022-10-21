1ca0d6280b69164f37635bacae8bc0d33ceb2398
Tom <brorkh@users.noreply.github.com>
2022-10-21 08:26:34 +0200

    Update export.php

--- export.php
+++ export.php
@@ -2,10 +2,6 @@
 // Database Connection
 require("db_connection.php");
 
-// get Users
-//$query = "SELECT id, zeit_in, zeit_out, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag FROM zeitdb ORDER BY id ASC";
-//$query = "SELECT (id = '1') AS id, zeit_in, zeit_out, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag FROM zeitdb ORDER BY id ASC";
-
 $query = "SELECT id, zeit_in, zeit_out, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, ((TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) - INTERVAL 8 HOUR) AS zeit_us FROM zeitdb ORDER BY id ASC";
 
 if (!$result = mysqli_query($con, $query)) {
