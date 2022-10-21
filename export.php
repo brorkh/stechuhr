623a23a65329360b748be6c97459e7f287d99e73
Tom <brorkh@users.noreply.github.com>
2022-10-21 07:51:47 +0200

    Update export.php

--- export.php
+++ export.php
@@ -3,7 +3,11 @@
 require("db_connection.php");
 
 // get Users
-$query = "SELECT id,zeit_in,zeit_out, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff FROM zeitdb ORDER BY id ASC";
+//$query = "SELECT id, zeit_in, zeit_out, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag FROM zeitdb ORDER BY id ASC";
+//$query = "SELECT (id = '1') AS id, zeit_in, zeit_out, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag FROM zeitdb ORDER BY id ASC";
+
+$query = "SELECT id, zeit_in, zeit_out, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, ((TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) - INTERVAL 8 HOUR) AS zeit_us FROM zeitdb ORDER BY id ASC";
+
 if (!$result = mysqli_query($con, $query)) {
     exit(mysqli_error($con));
 }
@@ -18,7 +22,7 @@ if (mysqli_num_rows($result) > 0) {
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=Zeiten.csv');
 $output = fopen('php://output', 'w');
-fputcsv($output, array('Nr', 'Zeit_In', 'Zeit_Out', 'Zeit_Diff'));
+fputcsv($output, array('Nr', 'Zeit Beginn', 'Zeit Ende', 'Stunden','Std. abzgl. Mittag', 'Tag', 'ÜbSt'));
 
 if (count($users) > 0) {
     foreach ($users as $row) {
