31bb58741109f146a43ab17f3d7f907a2ad76982
Tom <brorkh@users.noreply.github.com>
2022-10-21 07:50:40 +0200

    Update zeitdb.sql

--- zeitdb.sql
+++ zeitdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 31. Aug 2022 um 11:06
+-- Erstellungszeit: 21. Okt 2022 um 07:48
 -- Server-Version: 5.7.37-nmm1-log
 -- PHP-Version: 7.4.30
 
@@ -31,15 +31,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `zeitdb` (
   `id` int(6) UNSIGNED NOT NULL,
   `zeit_in` datetime DEFAULT NULL,
-  `zeit_out` datetime DEFAULT NULL
+  `zeit_out` datetime DEFAULT NULL,
+  `zeit_diff` time NOT NULL,
+  `zeit_mittag` time NOT NULL,
+  `tag` tinytext NOT NULL,
+  `zeit_us` time NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Daten für Tabelle `zeitdb`
 --
 
-INSERT INTO `zeitdb` (`id`, `zeit_in`, `zeit_out`) VALUES
-(37, '2022-08-31 07:53:05', '0000-00-00 00:00:00');
+INSERT INTO `zeitdb` (`id`, `zeit_in`, `zeit_out`, `zeit_diff`, `zeit_mittag`, `tag`, `zeit_us`) VALUES
+(3, '2022-08-31 07:53:05', '2022-08-31 16:57:01', '09:03:56', '08:03:56', 'Mittwoch', '00:03:56'),
+(4, '2022-09-01 08:10:49', '2022-09-01 17:08:50', '08:58:01', '07:58:01', 'Donnerstag', '-00:01:59'),
+(1, '2022-08-29 07:28:03', '2022-08-29 18:00:49', '10:32:46', '09:32:46', 'Montag', '01:32:46');
 
 --
 -- Indizes der exportierten Tabellen
@@ -59,7 +65,7 @@ ALTER TABLE `zeitdb`
 -- AUTO_INCREMENT für Tabelle `zeitdb`
 --
 ALTER TABLE `zeitdb`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
