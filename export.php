38a12705bc41679c0e7c5251d5c62ef4c20adbd5
Tom <brorkh@users.noreply.github.com>
2023-03-06 19:00:55 +0100

    Update export.php
    make the output readable for excel (filter at the first line in excel)

--- export.php
+++ export.php
@@ -2,7 +2,7 @@
 // Database Connection
 require("db_connection.php");
 
-$query = "SELECT id, zeit_in, zeit_out, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, ((TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) - INTERVAL 8 HOUR) AS zeit_us FROM zeitdb ORDER BY id ASC";
+$query = "SELECT id, DATE_FORMAT(zeit_in,'%d.%m.%Y') AS datum, TIME(zeit_in) AS zeitin, TIME(zeit_out) AS zeitout, TIMEDIFF(zeit_out,zeit_in) AS zeit_diff, (TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) AS zeit_mittag, ELT(DAYOFWEEK(zeit_in), 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag') AS tag, ((TIMEDIFF(zeit_out,zeit_in) - INTERVAL 1 HOUR) - INTERVAL 8 HOUR) AS zeit_us FROM zeitdb ORDER BY id ASC";
 
 if (!$result = mysqli_query($con, $query)) {
     exit(mysqli_error($con));
@@ -14,15 +14,15 @@ if (mysqli_num_rows($result) > 0) {
         $users[] = $row;
     }
 }
-
+$delimiter = ';';
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=Zeiten.csv');
 $output = fopen('php://output', 'w');
-fputcsv($output, array('Nr', 'Zeit Beginn', 'Zeit Ende', 'Stunden','Std. abzgl. Mittag', 'Tag', 'ÜbSt'));
+fputcsv($output, array('Nr', 'Datum', 'Zeit Beginn', 'Zeit Ende', 'Stunden','Std. abzgl. Mittag', 'Tag', 'MehrSt'), ';');
 
 if (count($users) > 0) {
     foreach ($users as $row) {
-        fputcsv($output, $row);
+        fputcsv($output, $row, $delimiter);
     }
 }
 ?>
